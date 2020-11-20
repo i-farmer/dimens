3094bddae00be6c629e08dd1c2010408a1ddc85e
bei <1>
2020-11-20 16:23:45 +0800

    导入

---
+++ app/src/main/java/i/farmer/demo/dimens/DemoActivity.java
@@ -0,0 +1,19 @@
+package i.farmer.demo.dimens;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * @author i-farmer
+ * @created-time 2020/11/20 4:09 PM
+ * @description <p>
+ */
+public class DemoActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_demo);
+    }
+}
